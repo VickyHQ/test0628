cd35ac196573e3efe1312c0342576561a8f2fefb
VickyHQ <vicky.qi.huang@gmail.com>
2023-06-28 18:05:40 -0700

    message string

--- whatever.py
+++ whatever.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import streamlit as st
 
-header= st.beta_container()
-dataset = st.beta_container()
-features= st.beta_container()
-modelTraining= st.beta_container()
+header= st.container()
+dataset = st.container()
+features= st.container()
+modelTraining= st.container()
 
 with header:
     st.title('Welcome to my awesome data science project!')
-    
