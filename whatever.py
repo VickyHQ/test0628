8e98f6dd70c1cb257c860d2890567291d5d872bd
VickyHQ <vicky.qi.huang@gmail.com>
2023-06-28 17:48:54 -0700

    vickys first commit.

---
+++ whatever.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+
+header= st.beta_container()
+dataset = st.beta_container()
+features= st.beta_container()
+modelTraining= st.beta_container()
+
+with header:
+    st.title('Welcome to my awesome data science project!')
+    
